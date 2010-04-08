ed38eab46c06a3d87506b2a5beb2c3ac26c78f57
Darcy Brown <darcy@dbcodeproject.com>
2010-04-07 20:16:11 -0500

    moved symlink into deploy.rb

--- deploy.rb
+++ deploy.rb
@@ -10,7 +10,21 @@ namespace :deploy do
     restart
     web.enable
   end
+  
+  task :symlink_configs do
+    run "mkdir -p #{latest_release}/config/initializers"
+    run "mkdir -p #{shared_path}/config/initializers"
+    run "mkdir -p #{shared_path}/public/files"
+
+    run "ln -nfs #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
+    run "ln -nfs #{shared_path}/public/files #{latest_release}/public/files"
+    run "find #{shared_path}/config/initializers -name *.rb | "\
+        "xargs -i basename {} | "\
+        "xargs -i ln -nfs #{shared_path}/config/initializers/{} #{latest_release}/config/initializers/{}"
+  end
+  
 end
 
-after "deploy", "deploy:cleanup"
-after "deploy:long", "deploy:cleanup"
+after "deploy:symlink", "deploy:symlink_configs"
+after "deploy",         "deploy:cleanup"
+after "deploy:long",    "deploy:cleanup"
