c359577005d915570560239f224703addd77b72c
Darcy Brown <darcy@dbcodeproject.com>
2010-04-09 11:56:42 -0500

    added console and dbconsole (experimental)

--- connect.rb
+++ connect.rb
@@ -1,5 +1,27 @@
 namespace :connect do
+  desc "drop you in ssh on remote" 
   task :default do
     exec "ssh #{user}@#{domain}"
   end
+  
+  desc "remotely console" 
+  task :console, :roles => :app do
+    input = ''
+    run "cd #{current_path} && ./script/console #{rails_env}" do |channel, stream, data|
+      next if data.chomp == input.chomp || data.chomp == ''
+      print data
+      channel.send_data(input = $stdin.gets) if data =~ /^(>|\?)>/
+    end
+  end
+
+  desc "remotely dbconsole (experimental)" 
+  task :dbconsole, :roles => :app do
+    input = ''
+    run "cd #{current_path} && ./script/dbconsole #{rails_env}" do |channel, stream, data|
+      next if data.chomp == input.chomp || data.chomp == ''
+      print data
+      channel.send_data(input = $stdin.gets) if data =~ /^(mysql|    -)>/
+    end
+  end
+  
 end
