43bf40971dbe356acd4febc9697af8ab2170f17c
Darcy Brown <darcy@dbcodeproject.com>
2010-04-13 20:13:46 -0500

    added connect:tail recipe

--- connect.rb
+++ connect.rb
@@ -14,6 +14,15 @@ namespace :connect do
     end
   end
 
+  desc "tail rails log files" 
+  task :tail, :roles => :app do
+    run "tail -f #{shared_path}/log/#{rails_env}.log" do |channel, stream, data|
+      puts  # for an extra line break before the host name
+      puts "#{channel[:host]}: #{data}" 
+      break if stream == :err    
+    end
+  end
+
   desc "remotely dbconsole (experimental)" 
   task :dbconsole, :roles => :app do
     input = ''
