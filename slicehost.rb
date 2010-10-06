bc713fcfe82b910e99e65e47982409d523ac65ea
Darcy Brown <darcy@dbcodeproject.com>
2010-10-06 16:13:21 -0500

    fix for chown command not working when there was nothing in the directory

--- slicehost.rb
+++ slicehost.rb
@@ -138,8 +138,7 @@ namespace :slicehost do
     # run "touch .ssh/authorized_keys"
     sudo "echo \"ssh-dss AAAAB3NzaC1kc3MAAACBAIpXW8t1wJO40g4swruYOZm+16Yf5QrPUozaGgt1psrJ8SFRWb49jThX5x9ZVSRi1EKdPy6Z1Hh3gBdNNW0KlMYO0ao9ZtycnS4W2MEVhH9teCtkIVzfG2xWopHyYyWtdiinGVPyu7scxw1EJGXNo5PZ59jzdsRXJtAFZgFAC9RpAAAAFQDJa/0OjzXCvZ3gorE5h4/MoYb+rwAAAIBjDc+a8zltj7tIzweqlNNtdbBHb7nwHLkbvJl0zpLw5VCk1ohp/wSOK3MRkIMOgshLm+lEWRLe5htQh/64XFZdTr2QU0YFyIE/UaefJz0W6jdwqGGny0BdBO6QAH/OBHTk0tJF8QffB2Yj6JnZaF8abyv7/s4HtHC1JwLSp6S+nQAAAIBwqrOJPTPqP3oeriPEbKnTgFwKnBRKpz208Ya2JiCK31SL0/vU7ML7H1ays5unRPpYS46PY4yZDx91+wmqY6Rn/bCHHNk6OooJu+gsS0QoDAOFtCiyfTfFiKOU1+iBEP1aeOwCF39YNHojU/EgEVcKyoJ2YFDIVgG19MCBwHbj3Q== darcy@Technicraft.local\" >> /home/#{deploy_user}/.ssh/authorized_keys"
     sudo "chmod 600 /home/#{deploy_user}/.ssh/authorized_keys"
-    sudo "chown -R #{deploy_user}:#{deploy_user} /home/#{deploy_user}/*"
-    sudo "chown -R #{deploy_user}:#{deploy_user} /home/#{deploy_user}/.*"
+    sudo "chown -R #{deploy_user}:#{deploy_user} /home/#{deploy_user}"
   end
   
   task :setup_config do
