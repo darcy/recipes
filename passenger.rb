9dc5303e2c043ac03908c85051ee9a52d5cb37e7
Darcy Brown <darcy@dbcodeproject.com>
2015-05-31 09:56:29 -0500

    don't fail deploy if kickstart doesn't work

--- passenger.rb
+++ passenger.rb
@@ -1,15 +1,15 @@
 namespace :deploy do
-  
+
   desc "Restarting mod_rails with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{current_path}/tmp/restart.txt"
   end
-  
+
   [:start, :stop].each do |t|
     desc "#{t} task is a no-op with mod_rails"
     task t, :roles => :app do ; end
   end
-  
+
   # task :long do
   #   transaction do
   #     update_code
@@ -17,18 +17,18 @@ namespace :deploy do
   #     symlink
   #     migrate
   #   end
-  # 
+  #
   #   restart
   #   web.enable
   #   cleanup
   # end
-  
+
 end
 
 namespace :passenger do
   desc "Kickstart app"
   task :kickstart do
-    run "curl -I http://#{web_domain}/"
+    run "curl -I http://#{web_domain}/ || true"
   end
 end
 
